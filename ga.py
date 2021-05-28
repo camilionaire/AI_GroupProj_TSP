ae7678191918e3cc3afcbdfcab27f2b77cc26104
YufeiT <keepaway46YF>
2021-05-28 09:51:00 -0700

    Added plotting best score history. Also added a extra condition in parents selection phase to avoid abnormal low score caused by city duplication in a chromosome

--- ga.py
+++ ga.py
@@ -9,7 +9,8 @@ class Population:
         self.pop = pop
         self.parents = []
         self.scores = []
-        self.best = 1000
+        self.best = float('inf')
+        self.result_history = []
         self.result = []
         self.adjacency_mat = adjacency_mat
 
@@ -19,12 +20,14 @@ class Population:
 
     def fitness(self, chromosome):
         sum_fit = 0
+        # clear previous generation's weights
         self.scores = []
         for i in range(len(chromosome) - 1):
             sum_fit += self.adjacency_mat[chromosome[i]][chromosome[i + 1]]
         sum_fit += self.adjacency_mat[chromosome[len(chromosome) - 1]][chromosome[0]]
         if self.best > sum_fit and len(chromosome) == len(set(chromosome)):
             self.best = sum_fit
+            self.result_history.append(sum_fit)
             self.result = chromosome.copy()
             print("\n-------------------------------------")
             print("Best score so far: ", self.best)
@@ -49,7 +52,12 @@ class Population:
         print("============================= \n")
 
     def select_parents(self):
-        return random.choices(self.pop, self.scores)[0]
+        candidate = random.choices(self.pop, self.scores)[0]
+        # reduce parents has low score because duplicate cities
+        while len(candidate) - len(set(candidate)) > len(self.pop[0]) * 0.4:
+            candidate = random.choices(self.pop, self.scores)[0]
+
+        return candidate
 
     def crossover_mutate(self):
         children = []
@@ -76,21 +84,25 @@ class Population:
         self.pop = children.copy()
         return self.pop
 
+    def plot_results_score(self):
+        plt.plot(range(len(self.result_history)), self.result_history, color="skyblue")
+        plt.show()
+
 
 def ga_main(my_table):
     start = time.time()
-    print(my_table)
-    pop = []
+    # print(my_table)
     cities = []
     population = Population([], my_table)
     for i in range(len(my_table)):
         cities.append(i)
 
-    pop = population.generate_population(cities, my_table, 1000)
-    for i in range(50):
-        print("Iteration: ", i)
+    pop = population.generate_population(cities, my_table, 100)
+    for i in range(1000):
+        print("Iteration: ", i) if i % 100 == 0 else None
         population.evaluate()
         population.crossover_mutate()
     population.get_best()
+    population.plot_results_score()
     elapsed = time.time() - start
     print("\n\nScript execution time: {} seconds".format(elapsed))
