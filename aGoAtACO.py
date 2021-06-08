da3f941eb1d2e6cba72e20d0d142a25ffc1951a5
Camilo <camilo3@pdx.edu>
2021-06-07 17:50:24 -0700

    initial set up

---
+++ aGoAtACO.py
@@ -0,0 +1,23 @@
+################################################################################
+# This will be an attempt at a functional ant colony optimization function
+
+from tools import *
+import numpy as np
+import random
+
+################################################################################
+# ETA is computed at beginning of function from table & distances between cities
+# TAO is set at beginning based on size of table, init to all 0's (no ants!)
+# both ETA and TAO are tables that will be the size of the table examined.
+ANTS = 10
+ITERS = 100
+ALPHA = 1
+BETA = 1
+RHO = .1
+
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
