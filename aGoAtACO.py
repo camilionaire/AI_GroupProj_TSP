8f2e986c2bb530b0661121b39a823fe211aba7af
Camilo <camilo3@pdx.edu>
2021-06-07 22:01:45 -0700

    ants seem to be travelling... seem to be

--- aGoAtACO.py
+++ aGoAtACO.py
@@ -11,7 +11,8 @@ import random
 # both ETA and TAO are tables that will be the size of the table examined.
 ANTS = 10
 ITERS = 100
-INIT_PHER = 1
+INIT_PHER = 1 # init put on tao
+PHEROMONES = 2 # pher put down along path like Q?...
 RHO = .1
 ALPHA, BETA = 1, 1
 TITLE = './datasets/five19.txt'
@@ -48,8 +49,27 @@ def bigBad(tao, eta, size):
 
     return prob
 
-    
+def makeTravelChoices(ant, prob, size):
+    while len(ant) < size:
+        currCity = ant[len(ant) - 1]
+        possibilities = []
+        poss_prob = []
+        sum_pos = 0
+        for col in range(0, size): # we go through cities
+            # sees if the city is a possibility
+            if col not in ant: # matches city to tao*eta func
+                possibilities.append(col)
+                poss_prob.append(prob[currCity][col])
+                sum_pos += prob[currCity][col]
 
+    # normalization quotient
+        poss_prob = poss_prob / sum_pos
+        # why do I need to put zero at the end here?...
+        choice = random.choices(possibilities, poss_prob)[0]
+        # for testing that things were getting selected correctly
+        # print("Choices:", possibilities, "Probs:", poss_prob)
+        ant.append(choice)
+        
 def main():
     table = np.loadtxt(TITLE)
     size = table.shape[0] #finds size of map
@@ -61,12 +81,28 @@ def main():
     print("TAO:\n", tao)
     print("TOP PROB:\n", top_prob)
     
-    for i in range(0, 20):
+    # for the number of... eventually iterations
+    for i in range(0, 2):
         colony = []
+        fitness = []
+
+    # all of the ants travel here
         for i in range(0, ANTS):
             ant = createAnt(size)
             colony.append(ant)
         print("Colony: ", colony)
+        for ant in colony:
+            makeTravelChoices(ant, top_prob, size)
+            fitness.append(findTourLen(ant, table))
+            
+        # we need to distribute and evaporate here
+        # deltaTao = np.zeros((size, size))
+        # for ant in colony:
+
+
+        print("Colony after travel: ")
+        for ant in range(0, ANTS):
+            print(colony[ant], "tourLength:", fitness[ant])
 
 if __name__ == "__main__":
     main()
